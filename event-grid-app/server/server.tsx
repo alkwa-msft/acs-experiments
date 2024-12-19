ef92e5bab0e5fbe2a20a5d22750879e41fb3315b
Alex Kwan <alkwa@microsoft.com>
2024-12-19 09:52:24 -0800

    pushing up changes with additional debugging for the socketio connection

--- event-grid-app/server/server.tsx
+++ event-grid-app/server/server.tsx
@@ -2,10 +2,10 @@ import express from 'express';
 import path from 'path';
 import { CommunicationIdentityClient } from '@azure/communication-identity';
 import cors from 'cors';
-const settings = require('../appsettings.json');
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
-const app = express();
-const port = process.env.PORT || 3001;
+const settings = require('../appsettings.json');
 
 const connectionString = process.env.ACS_CONNECTION_STRING || settings.resourceConnectionString;
 
@@ -15,10 +15,36 @@ if (connectionString === undefined) {
 
 let identityClient = new CommunicationIdentityClient(connectionString);
 
+const app = express();
+const port = process.env.PORT || 3001;
 // Use express.json() middleware to parse JSON request bodies
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../build')));
+const server = createServer(app);
+const io = new Server(server);
+
+console.log('about to set up my socket server');
+io.on('connection', (socket) => {
+  console.log('[ws] A user connected');
+  socket.on('message', (message) => {
+    console.log('[ws] Received message:', message);
+    // Broadcast the message to all connected clients
+  });
+  socket.on('close', () => {
+    console.log('[ws] A user disconnected');
+  });
+});
+console.log('[ws] I have registered all of my events for my wssocket server');
+
+
+setInterval(()=>{
+  if(io.engine.clientsCount > 0){
+    console.log('[ws] sending something now ', io.engine.clientsCount)
+    io.emit('message', 'Hello from the server');
+    io.send('[ws] Hello from the server', io.engine.clientsCount);
+  }
+}, 5000);
 
 app.post('/token', async (req, res) => {
   try {
@@ -37,6 +63,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// important! must listen from `server`, not `app`, otherwise socket.io won't function correctly
+server.listen(port, () => console.info(`Listening on port ${port}.`));
--- event-grid-app/src/index.tsx
+++ event-grid-app/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import io from 'socket.io-client';
 
 const root = document.getElementById('root');
 
@@ -20,3 +21,30 @@ if (rootReactElement) {
 } else {
   console.error('Failed to find root element');
 }
+
+ // if (socket === null) {
+    // console.log("[ws] Socket was null so we need to create one")
+    const newSocket = io('http://localhost:3001');
+    newSocket.on('connect_error', (err: any) => {
+      console.error('Connection error:', err.message);
+      console.error('Error description:', err.description);
+      console.error('Error context:', err.context);
+    });
+
+    console.log('[ws] registered for connect event')
+    console.log('this is my new socket ', newSocket);
+    newSocket.on('connect', () => {
+      console.log('[ws] connected to server');
+    });
+
+    console.log('[ws] registered for message event')
+    newSocket.on('message', (msg: string) => {
+      console.log('[ws] message from server: ' + msg);
+    });
+    
+    // console.log('[ws] registered for disconnect event')
+    // newSocket.on('disconnect', () => {
+    //   console.log('[ws] disconnected from server');
+    // });
+    /// setSocket(newSocket);
+  // }
